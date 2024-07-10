6f5d976b140a18d87b8656899f91d4c173499469
Johab Freitas <freitasjohab@gmail.com>
2024-07-10 15:48:15 -0300

    Criado o LinkController

---
+++ src/main/java/com/rocketseat/planner/link/LinkController.java
@@ -0,0 +1,51 @@
+package com.rocketseat.planner.link;
+
+
+import com.rocketseat.planner.trip.Trip;
+import com.rocketseat.planner.trip.TripRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/trips")
+public class LinkController {
+
+    @Autowired
+    private LinkService linkService;
+
+    @Autowired
+    private LinkRepository repository;
+
+    @Autowired
+    private TripRepository tripRepository;
+
+    @GetMapping("/{tripId}/links")
+    public ResponseEntity<List<LinkData>> getAllLinks(@PathVariable UUID tripId){
+        List<LinkData> linkDataList = this.linkService.getAllLinksFromId(tripId);
+
+        return ResponseEntity.ok(linkDataList);
+    }
+
+    @PostMapping("/{id}/links")
+    public ResponseEntity<LinkResponse> registerLink(@PathVariable UUID id, @RequestBody LinkRequestPayload payload){
+        Optional<Trip> trip = this.tripRepository.findById(id);
+
+        if(trip.isPresent()){
+            Trip rawTrip = trip.get();
+
+            LinkResponse linkResponse = this
+                    .linkService
+                    .registerLink(payload, rawTrip);
+
+            return ResponseEntity.ok(linkResponse);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+}
