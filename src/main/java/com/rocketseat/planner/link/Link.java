87cd4546ae04ae7db36e2fe3ed5b784cd7c5b39c
Johab Freitas <freitasjohab@gmail.com>
2024-07-10 09:28:19 -0300

    Criada entidade que irá representar um Link

---
+++ src/main/java/com/rocketseat/planner/link/Link.java
@@ -0,0 +1,32 @@
+package com.rocketseat.planner.link;
+
+import com.rocketseat.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "links")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Link {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+}
